1102ce530e08d687a02e71a49f07749edef375f8
matthew8541 <ytseng35@gatech.edu>
2021-07-04 21:34:49 +0800

    intro page button added

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,16 +1,30 @@
 // import classes from "./Intro.module.css"
 import React, { useEffect } from 'react'
 import classes from "./Intro.module.css"
+import { Button } from '@material-ui/core';
+import { NavLink } from 'react-router-dom';
 
 const Intro = () => {
   useEffect(() => {
     document.body.className = "intro-body";
   }, [])
-  
+
   return (
-    <div className={classes.intro}>
-      <h1 className="centered">Welcome to MinimalTypist</h1>
-      <h3 className="centered">Enjoy the minimalistic typing environment</h3>
+    <div className={`${classes.intro} centered`}>
+      <h1>Welcome to MinimalTypist</h1>
+      <h3>Enjoy the minimalistic typing environment</h3>
+      <Button
+        variant="contained"
+        color="inherit"
+      >
+        <NavLink to="/home" style={{
+          textDecoration: "none",
+          color: "#000000"
+        }}
+        >
+          Start Typing
+        </NavLink>
+      </Button>
     </div>
   );
 };
