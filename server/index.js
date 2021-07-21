1cb45d1261639b69f969e5361ade84ee796debd4
matthew8541 <ytseng35@gatech.edu>
2021-07-21 20:56:10 +0800

    Deploy Config

--- server/index.js
+++ server/index.js
@@ -25,22 +25,12 @@ db.once("open", () => {
 app.use(cors());
 app.use(express.json({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.use("/", userRouter);
 app.use("/", profileRouter);
 
-// app.get('/', (req, res) => {
-//   res.send('Welcome to MinimalTypist')
-// })
-
-// app.get('/home', (req, res) => {
-//   res.send('Here is home')
-// })
-
-// Handles any requests that don't match the ones above
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get('*', (req, res) => {
-  // console.log("--> GET *")
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
